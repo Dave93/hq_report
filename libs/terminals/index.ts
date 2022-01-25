32caf71bfcc8f08a705b89c06e798464222eb1c8
Davron Yuldashev <yul.davron.93@gmail.com>
2022-01-25 09:22:41 +0500

    fixes

--- libs/terminals/index.ts
+++ libs/terminals/index.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 import getConfig from "next/config";
 import { useSession, signIn, signOut, getSession } from "next-auth/react";
-import { Session } from "@types/session";
 
 const { publicRuntimeConfig } = getConfig();
 let webAddress = publicRuntimeConfig.apiUrl;
 
 export const fetchAll = async () => {
-  const session: Session = await getSession();
+  const session: any = await getSession();
   let config = {
     headers: {
       Authorization: "Bearer " + session.user.accessToken,
